da646fbf0beafb51332f3a15632cd4b2b50a86e1
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-10-01 03:34:13 +0600

    update

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,32 +1,40 @@
 import React from "react";
 import EarthModel from "../../components/EarthModel/EarthModel";
-import SciFiText from "../../components/SciFiText/SciFiText";
 import InfoBoxLeft from "../../components/InfoBox/InfoBoxLeft/InfoBoxLeft";
 import InfoBoxRight from "../../components/InfoBox/InfoBoxRight/InfoBoxRight";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="relative h-screen w-full overflow-hidden bg-[#1c1c1e] text-white font-mono">
-      {/* Sci-Fi Text Section */}
-  
-
+    <div className="relative w-full bg-[#1c1c1e] text-white font-mono">
       {/* Earth Model */}
-      <div className="flex items-center justify-center relative h-full">
+      <div className="earth-container">
         <EarthModel />
+      </div>
 
-        {/* Info boxes */}
-        <div className="absolute top-1/3 left-0 ml-8">
+      {/* Info Section */}
+      <div className="info-section relative z-10 pt-8">
+        {/* Info Box 1 */}
+        <div className="flex justify-between mb-8 px-8">
           <InfoBoxLeft />
+          <InfoBoxRight />
         </div>
-
-        <div className="absolute top-2/3 right-0 mr-8">
+        {/* Info Box 2 */}
+        <div className="flex justify-between mb-8 px-8">
+          <InfoBoxLeft />
+          <InfoBoxRight />
+        </div>
+        {/* Info Box 3 */}
+        <div className="flex justify-between mb-8 px-8">
+          <InfoBoxLeft />
           <InfoBoxRight />
         </div>
       </div>
 
-      {/* Subtle Bottom Gradient */}
-      <div className="absolute bottom-0 w-full h-32 bg-gradient-to-t from-black to-transparent"></div>
+      {/* Background Space Image */}
+      <div className="space-background">
+        {/* Optional: you can add a space background image or more content here */}
+      </div>
     </div>
   );
 };
