e980041f4a10c203f2dd95ead49ed98c4b70abbd
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-10-03 04:27:10 +0600

    update

--- frontend/src/pages/FloodMap/FloodMap.js
+++ frontend/src/pages/FloodMap/FloodMap.js
@@ -3,7 +3,7 @@ import "./FloodMap.css";
 
 const FloodMap = () => {
   return (
-    <div className="container mx-auto p-6 mt-[-80px] text-gray-200 bg-gray-900 min-h-screen relative">
+    <div className="floodMapContainer mx-auto p-6 mt-[-80px] text-gray-200 bg-gray-900 min-h-screen relative">
       <section className="map-container mt-16">
         {/* Alert Button */}
         <a
@@ -24,7 +24,7 @@ const FloodMap = () => {
         {/* Wrapper div for the iframe and the overlay */}
         <div className="iframe-container mb-8">
           <h2 className="text-2xl font-semibold mb-4 text-green-400">
-            ArcGIS Flood Map
+            SKY FARM ANALYTICS
           </h2>
           {/* Embedded ArcGIS map as an iframe */}
           <iframe
