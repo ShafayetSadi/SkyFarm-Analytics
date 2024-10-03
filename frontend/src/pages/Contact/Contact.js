1c5b9735be26f840cc520cd261b9df3e1c8985c6
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-10-03 17:55:08 +0600

    update

--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -1,10 +1,27 @@
-// pages/Contact.js
 import React from "react";
+import { Link } from "react-router-dom"; // Ensure you have react-router-dom for navigation
 
 const Contact = () => {
   return (
-    <div className="container pt-32 mx-auto p-4 bg-gray-950 min-h-screen">
-      <h1 className="text-3xl font-bold text-center mb-8 text-cyan-400">
+    <div className="container mx-auto pt-32 p-6 bg-dark-futuristic min-h-screen relative">
+      <Link to="/" className="home-icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-home"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 9l9 -6l9 6v11a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-7h-4v7a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2z" />
+        </svg>
+      </Link>
+      <h1 className="text-3xl font-bold text-center mb-8 text-neon-blue glow-effect">
         Contact Us
       </h1>
       <form className="max-w-md mx-auto bg-gray-800 p-6 rounded-lg shadow-lg">
