86e7bdaf592f8db3bde9b6966cd6aa814bb029be
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-10-04 11:16:28 +0600

    update

--- frontend/src/pages/FloodMap/FloodMap.js
+++ frontend/src/pages/FloodMap/FloodMap.js
@@ -2,7 +2,7 @@ import React from "react";
 import Menu from "../../components/Menu/Menu"; // Import the Menu component
 import "./FloodMap.css";
 import SkyFarmBot from "../SkyFarmBot/SkyFarmBot"; // Import the SkyFarmBot component
-const FloodMap = () => {
+const FloodMap = () => { 
   return (
     <div className="floodMapContainer mx-auto p-6 text-gray-200 bg-gray-900 min-h-screen relative">
       {/* Hamburger Menu */}
